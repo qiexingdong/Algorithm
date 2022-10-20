a610c9c138aca4a803597d8a2d3e547fb07cfc92
Penguin winter <98592880+qiexingdong@users.noreply.github.com>
2022-10-20 21:21:55 +0800

    Create 二进制中1的个数.cpp

---
+++ 剑指offer/位运算/二进制中1的个数.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+     int  NumberOf1(int n) {
+         int flag = 0;
+        for(int i = 0;i<32;i++){
+            if(n&(1<<i))
+            flag++;
+        }
+         return flag;
+     }
+};
